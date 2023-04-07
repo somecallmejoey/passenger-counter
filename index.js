22d51e610f196262ad8da7adc0f65878a1e04f0d
Joey G <121167275+somecallmejoey@users.noreply.github.com>
2023-04-07 12:19:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+}
+
+function zero() {
+  let zero = (count = 0);
+  countEl.textContent = zero;
+}
